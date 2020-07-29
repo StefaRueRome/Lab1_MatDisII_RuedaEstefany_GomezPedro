efe92bf80f750056d7c21a93c6309303e7e99da2
Estefany Rueda <yurley.rueda.2019@upb.edu.co>
2020-07-29 11:26:55 -0500

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada=new Scanner(System.in);
+        int numCasos= entrada.nextInt();
+
+        int cont=1;
+        while(cont<=numCasos) {
+
+            String cadena=entrada.nextLine();
+            if (cadena.isEmpty() == true) {
+                continue;
+            }
+            if (cont > 0) System.out.println();
+
+            int tamanno=cadena.length();
+            for (int periodo = 1; periodo < tamanno; periodo++) {
+                if ((tamanno % periodo) != 0) {
+                    continue;
+                }
+                int valor = tamanno / periodo;
+                String subCadena = cadena.substring(0, periodo);
+                String nCadena = "";
+                for (int k = 0; k < valor; k++) {
+                    nCadena += subCadena;
+                }
+                if (nCadena.equals(cadena) == true) {
+                    System.out.println(periodo);
+
+                }else{
+                    System.out.println(cadena.length());
+                    break;
+                }
+
+
+            }
+            cont++;
+        }
+
+    }
+}
