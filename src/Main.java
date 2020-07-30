c2174af7d89bb1aab9cf8a6a6fbdb4377798e9d5
Estefany Rueda <yurley.rueda.2019@upb.edu.co>
2020-07-29 22:16:32 -0500

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner entrada=new Scanner(System.in);
@@ -9,30 +8,38 @@ public class Main {
         while(cont<=numCasos) {
 
             String cadena=entrada.nextLine();
-            if (cadena.isEmpty() == true) {
+            if (cadena.isEmpty() == true)
+            {
                 continue;
             }
-            if (cont > 0) {
+            if (cont > 0)
+            {
                 System.out.println();
             }
             int tamanno=cadena.length();
-            for (int periodo = 1; periodo < tamanno; periodo++) {
-                if ((tamanno % periodo) != 0) {
+            for (int periodo = 1; periodo < tamanno; periodo++)
+            {
+                if ((tamanno % periodo) != 0)
+                {
                     continue;
                 }
                 int valor = tamanno / periodo;
                 String subCadena = cadena.substring(0, periodo);
                 String nCadena = "";
-                for (int k = 0; k < valor; k++) {
+                for (int k = 0; k < valor; k++)
+                {
                     nCadena += subCadena;
                 }
-                if (nCadena.equals(cadena) == true) {
+                if (nCadena.equals(cadena) == true)
+                {
                     System.out.println(periodo);
 
-                }else{
-                    System.out.println(cadena.length());
-                    break;
                 }
+                else
+                    {
+                        System.out.println(cadena.length());
+                        break;
+                    }
 
 
             }
